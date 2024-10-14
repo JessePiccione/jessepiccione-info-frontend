b12266ca6b08a74613a54ccd192c826caa24bd4f
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-10-14 14:05:42 +0000

    started working on navlist to add more dynamic screen behavior

--- src/components/body/main/nav/NavList.js
+++ src/components/body/main/nav/NavList.js
@@ -2,7 +2,7 @@ import NavItem from './navItem.js'
 const viewNames = ['Home','Experience', 'Education', 'Projects', 'Skills', 'Awards']
 function NavList(){
     return (
-        <ul className="navbar-nav d-flex flex-row justify-content-between me-auto align-self-bottom px-2">
+        <ul className="navbar-nav d-flex flex-col flex-lg-row justify-content-between me-auto align-self-bottom px-2">
             {viewNames.map((val,index)=> {
                 let i = index;
                 return ( 
