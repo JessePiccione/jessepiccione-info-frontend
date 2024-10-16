6d2367af53bb7fda5d3b160d3cc56deaa589337c
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-10-16 13:41:40 +0000

    linted and reduced apploader.js

--- src/components/AppLoader.js
+++ src/components/AppLoader.js
@@ -12,36 +12,28 @@ import Footer from './body/footer/Footer.js'
 import HeaderTwo from './body/partials/headerTwo.js'
 function AppLoader() {
   const [loadingState, setLoadingState] = useState({loading:true,error:null});
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
   useEffect(()=>{
     const wakeUpCall = async () =>{
-      if(!ignore){
-        try{
-          const request = await fetch('https://jessepiccione-info-backend.ue.r.appspot.com/')//'https://literate-space-enigma-6wq4rw6577rfrr4j-8080.app.github.dev/'
-          await request.text()
-        }
-        catch (error) {
-          setLoadingState(error)
-        }
-        finally{
-          await new Promise(resolve=> setTimeout(resolve, 2000))
-          setLoading(false)
-        }
+      try{
+        const request = await fetch('https://jessepiccione-info-backend.ue.r.appspot.com/')//'https://literate-space-enigma-6wq4rw6577rfrr4j-8080.app.github.dev/'
+        await request.text()
+      }
+      catch (error) {
+        setLoadingState(()=>{return {...loadingState, error:error}})
+      }
+      finally{
+        await new Promise(resolve=> setTimeout(resolve, 2000))
+        setLoadingState({...loadingState, loading:false})
       }
     }
-    let ignore = false
     wakeUpCall();
-    return () => {
-      ignore=true
-    }
   },[])
-  if(loading || error) return(
+  if(loadingState.loading || loadingState.error) return(
     <div className='row app'>
       <div className="col-12 d-flex primary-color bg-gradient" style={{height:'100vh'}}>
         <div className='col-10 col-xxl-6 m-auto'>
           <Card>
-            <HeaderTwo title={(error)?error.message:'Initiating Backend Services...'}/>
+            <HeaderTwo title={(loadingState.error)?loadingState.error.message:'Initiating Backend Services...'}/>
             <img className='img-fluid m-auto rounded-profile rotate' style={{
               minHeight:'250px',
               minWidth:'250px'
