143c94ef7ae0ac93958056132b9b541806ca5f62
JessePiccione <jessepiccione@yahoo.com>
2024-10-21 21:41:12 -0400

    stuff for loader

--- src/components/AppLoader.js
+++ src/components/AppLoader.js
@@ -12,23 +12,21 @@ import Footer from './body/footer/Footer.js'
 import HeaderTwo from './body/partials/headerTwo.js'
 function AppLoader() {
   const [loadingState, setLoadingState] = useState({loading:true,error:null});
-  useEffect(()=>{
-    const wakeUpCall = async () =>{
-      try{
-        const request = await fetch('https://jessepiccione-info-backend.ue.r.appspot.com/')//'https://literate-space-enigma-6wq4rw6577rfrr4j-8080.app.github.dev/'
-        await request.text()
-      }
-      catch (error) {
-        setLoadingState(()=>{return {...loadingState, error:error}})
-      }
-      finally{
-        await new Promise(resolve=> setTimeout(resolve, 2000))
-        setLoadingState({...loadingState, loading:false})
-      }
+  const wakeUpCall = async () =>{
+    try{
+      const request = await fetch('https://jessepiccione-info-backend.ue.r.appspot.com/')//'https://literate-space-enigma-6wq4rw6577rfrr4j-8080.app.github.dev/'
+      await request.text()
     }
-    wakeUpCall();
-  },[])
-  if(loadingState.loading || loadingState.error) return(
+    catch (error) {
+      setLoadingState({...loadingState, error:error})
+    }
+    finally{
+      await new Promise(resolve=> setTimeout(resolve, 500))
+      setLoadingState({...loadingState, loading:false})
+    }
+  }//eslist-disable-next-line
+  useEffect(()=>{wakeUpCall()},[])
+  return(loadingState.loading || loadingState.error)?(
     <div className='row app'>
       <div className="col-12 d-flex primary-color bg-gradient" style={{height:'100vh'}}>
         <div className='col-10 col-xxl-6 m-auto'>
@@ -42,8 +40,7 @@ function AppLoader() {
         </div>
       </div>
     </div>
-  )
-  return (
+  ):(
     <div className="App">
       <VantaBackground/>
       <APIUrlProvider>
